83d3546c387b320ed4672072f287c0ad3604725e
Ivan Bodnarash <bodnarash1999@gmail.com>
2025-07-09 00:34:49 +0200

    Adding hint pic

--- src/components/HintComponent.jsx
+++ src/components/HintComponent.jsx
@@ -1,7 +1,10 @@
+import hintPic from "../assets/Hint.png";
+
 export default function HintComponent() {
   return (
     <div className="p-8 flex justify-center">
-      <img src="https://placehold.co/600x400" alt="" />
+      <img src={hintPic} alt="hint" className="w-96" />
+      {/* <img src="https://placehold.co/600x400" alt="" /> */}
     </div>
   );
 }
