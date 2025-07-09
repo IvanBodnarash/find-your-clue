583c814a6baf7bb12e61038ef0620e668e170ce0
Ivan Bodnarash <bodnarash1999@gmail.com>
2025-07-09 10:55:58 +0200

    Changes2

--- src/components/HintComponent.jsx
+++ src/components/HintComponent.jsx
@@ -3,7 +3,7 @@ import hintPic from "../assets/Hint.png";
 export default function HintComponent() {
   return (
     <div className="p-8 flex justify-center">
-      <img src={hintPic} alt="hint" className="w-96" />
+      <img src="/Hint.png" alt="hint" className="w-96" />
       {/* <img src="https://placehold.co/600x400" alt="" /> */}
     </div>
   );
